49ba43b4c450525b9badbc3d4925db803a877aa5
Harini Janakiraman <harinijanakiraman@gmail.com>
2016-05-20 22:43:03 +0800

    Day 13: AI ChatBot for Messenger

---
+++ Day 13/README.md
@@ -0,0 +1,10 @@
+100DaysOfCode Challenge
+
+#DAY 13:  AI ChatBot for Messenger
+
+- Creating a ChatBot for messenger with a simple AI logic to respond to messages. 
+- The messenger bot was created using Node then deployed and hosted on Heroku.
+
+Read more about it on medium here: https://medium.com/@hmovielabs
+
+About me and other interesting projects on my website: http://HMovieLabs.com/
---
+++ Day 13/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+// GET to check if server is up and running
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
+
+// GET for Facebook Webhook
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'fb_ai_chatbox') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
