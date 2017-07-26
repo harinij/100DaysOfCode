7cee9328feb25b1bcdf7ba21a8dc4d74d9e62f07
Harini Janakiraman <harinijanakiraman@gmail.com>
2017-07-26 18:52:19 +1000

    Day 22: AI Game Bot using Open AI Gym and Universe

--- Day 22/gamebot_basic.py
+++ Day 22/gamebot_basic.py
@@ -1,11 +1,11 @@
 import gym
 import universe # register the universe environments
-env = gym.make('flashgames.NeonRace-v0')
+env = gym.make('flashgames.NeonRace-v0') # You can run many environment in parallel
 env.configure(remotes=1) # automatically creates a local docker container
-observation_n = env.reset()
+observation_n = env.reset() # Initiate the environment and get list of observations of its initial state
 while True:
  action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n] # your agent here
- observation_n, reward_n, done_n, info = env.step(action_n)
- print ("observation: ", observation_n)
- print ("reward: ", reward_n)
- env.render()
+ observation_n, reward_n, done_n, info = env.step(action_n) # Reinforcement Learning action by agent
+ print ("observation: ", observation_n) # Observation of the environment
+ print ("reward: ", reward_n) # If the action had any postive impact +1/-1
+ env.render() # Run the agent on the environment
